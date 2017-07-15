7f2df019d7dbd50b4528f4ee61fbb694d733813c
Daniel Kahn Gillmor <dkg@fifthhorseman.net>
2017-07-15 11:56:20 +0200

    remove comments that are not necessary now that we have clean indenting

--- background.js
+++ background.js
@@ -7,18 +7,18 @@ function listener(highlightInfo){
 	
 	xhttp.onreadystatechange = function() {
 	    if (this.readyState == 4) {
-		if (this.status == 451){ //if error 451, do something
+		if (this.status == 451) {
 		    chrome.browserAction.setIcon({
 			path : "icons/451/icon_128.png"
 		    });
-		}//endif this.status
+		}
 		else { //no censorship detected, do something
 		}
-	    }//end of readystate
-	};//end of onready
+	    }
+	};
 	
 	xhttp.open("GET", Url, true);
 	xhttp.send();
-    });//end of chrome.tabs.query
-}//end of function listener
+    });
+}
 chrome.tabs.onHighlighted.addListener(listener);
